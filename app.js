47a5a8c35c791dd6af21c949c423bac16982f496
Alihasan06 <alihassanraja06@gmail.com>
2024-12-11 07:17:23 -0800

    commit

--- app.js
+++ app.js
@@ -4,10 +4,14 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
 app.get('/', (req, res) => {
-    res.cookie("name","Ali")
-    res.send('Hello World!');   
+    res.cookie("name", "Ali Hasan")
+    res.send('Hello Backend!');
+}); 
+app.get('/read', (req, res) => {
+ 
+    res.send('Hello World!');
 });
 
-app.listen(3000, () => {        
+app.listen(3000, () => {
     console.log('Example app listening on port 3000!');
 });
