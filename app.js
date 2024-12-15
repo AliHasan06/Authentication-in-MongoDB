985b623ad43ab93af818e4c0e94e670a2db73b1b
Alihasan06 <alihassanraja06@gmail.com>
2024-12-15 03:36:31 -0800

    learning jwt

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+const jwt = require('jsonwebtoken')
 const cookieParser = require('cookie-parser');
 
 app.use(cookieParser())
